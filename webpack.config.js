74992ef346bbc4782d88d64af262a88fa023f447
Frederico Nogueira <fredericonogueira01@outlook.com>
2024-04-17 07:09:50 -0300

    Criando um servidor local de desenvolvimento e configurando o loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "transpiled", "index.js"),
+  entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "bundle[hash].js"
+    filename: "bundle[fullhash].js"
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
     }),
     new CleanWebpackPlugin(),
-  ]
+  ],
+  module: {
+    rules:[{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }]
+  },
+  devServer: {
+    port: 3000
+  }
 };

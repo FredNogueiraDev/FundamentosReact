9d8164e70dd58f3ed1db8957a9b79861d888a7f0
Frederico Nogueira <fredericonogueira01@outlook.com>
2024-04-18 06:46:29 -0300

    React Fragment

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <h1>Novo arquivo meu chapa</h1>
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
   module: {
     rules:[{
-      test: /\.js$/,
+      test: /\.jsx?$/,
       exclude: /node_modules/,
       use: 'babel-loader'
     }]

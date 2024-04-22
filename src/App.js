d18e5426fdad9d60a6c37f006d67cfd731007ffa
Frederico Nogueira <fredericonogueira01@outlook.com>
2024-04-22 07:00:15 -0300

    Trabalhando com States

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { ThemeProvider } from "./ThemeContext";
 
 const category = "Categoria fundamentos do React";
 
-// O React obriga que todo componente inicie com a primeira letra em maiúsculo
 function App() {
   const [posts, setPosts] = useState([
     { id: Math.random(), likes: 10, title: "Título da notícia #01", subtitle: "Subtítulo da notícia #01", read: false },
@@ -22,7 +21,8 @@ function App() {
           id: Math.random(),
           likes: 25,
           title: `Título da notícia #${prevState.length + 1}`,
-          subtitle: `Subtítulo da notícia #${prevState.length + 1}`
+          subtitle: `Subtítulo da notícia #${prevState.length + 1}`,
+          read: false
         }
       ]);
     }, 2000);
@@ -48,7 +48,6 @@ function App() {
 
       {posts.map(post => (
         <Post
-          // Sempre que precisarmos renderizar uma lista é necessário passar uma key, é uma propriedade que o React já tem
           key={post.id}
           onRemove={handleRemovePost}
           post={post}
